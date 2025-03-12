910bf461cb00b91598693e4a4949d56c7919ad03
Donald Turnworth <donaldturnworth@Donalds-MacBook-Pro.local>
2025-03-11 19:30:30 -0600

    first crack at threadsafety
    A little confused about the documentation of the two ordering constraints (for success case and failure case): https://doc.rust-lang.org/std/sync/atomic/struct.AtomicBool.html#method.compare_exchange_weak And, I have no idea how to test this, so I'm really not sure it works.

--- src/simplelogger.rs
+++ src/simplelogger.rs
@@ -2,15 +2,25 @@ use std::fs::File;
 
 use std::io::Write;
 
-static mut MY_FILE_OPT: Option<File> = None;
+use std::sync::atomic::{AtomicBool, Ordering};
 
-//XXXXXX gotta add thread-safety!
+
+
+static LOCK: AtomicBool = AtomicBool::new(false);
+
+static mut MY_FILE_OPT: Option<File> = None;
 
 pub fn write(fname: &str, bs: &[u8]) {
     unsafe {
+	// Spin until this thread gets the exclusive ownership of LOCK:
+	while LOCK.compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed).unwrap() { }
+
 	#[allow(static_mut_refs)]
 	let f = MY_FILE_OPT.get_or_insert_with(||
 					   File::create(fname).unwrap());
 	f.write_all(bs).unwrap();
+
+	// ok we're done, release the lock
+	LOCK.store(false, Ordering::Release);
     }
 }
