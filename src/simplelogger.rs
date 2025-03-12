7649a868b19ebedc8f33a47b210ded6333a6835d
Donald Turnworth <donaldturnworth@Donalds-MacBook-Pro.local>
2025-03-11 21:17:21 -0600

    yep, the thread-safety was wrong
    (I was actually able to test it by running Zcash zebrad with smalloclog installed as the default global allocator, and seeing that the resulting log files were corrupted. At least I think that's what happened. Since I fixed the bugs I found in it and now the log files don't seem to get corrupted any more.)

--- src/simplelogger.rs
+++ src/simplelogger.rs
@@ -2,25 +2,14 @@ use std::fs::File;
 
 use std::io::Write;
 
-use std::sync::atomic::{AtomicBool, Ordering};
-
-
-
-static LOCK: AtomicBool = AtomicBool::new(false);
-
 static mut MY_FILE_OPT: Option<File> = None;
 
-pub fn write(fname: &str, bs: &[u8]) {
+pub fn write(bs: &[u8]) {
+    // This function is called only from within a lock (a dumb compare-and-exchange spinlock) held by smalloclog. So we can use non-atomic test and set on MY_FILE_OPT.
     unsafe {
-	// Spin until this thread gets the exclusive ownership of LOCK:
-	while LOCK.compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed).unwrap() { }
-
 	#[allow(static_mut_refs)]
 	let f = MY_FILE_OPT.get_or_insert_with(||
-					   File::create(fname).unwrap());
+					       File::create("smalloclog.log").unwrap());
 	f.write_all(bs).unwrap();
-
-	// ok we're done, release the lock
-	LOCK.store(false, Ordering::Release);
     }
 }
