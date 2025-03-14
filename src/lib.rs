c35f91ac13577f8dae09da96fb308a96692646d7
Donald Turnworth <donaldturnworth@Donalds-MacBook-Pro.local>
2025-03-14 15:28:27 -0600

    refactor logging, prettify output, bump version dep on smalloc

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,14 @@
 use std::fs::{File};
 use std::io::Write;
-use std::io;
 
-use std::primitive::usize;
+use bytesize::ByteSize;
 
-static mut MY_FILE_OPT: Option<File> = None;
-
-#[allow(static_mut_refs)]
-fn write(bs: &[u8]) -> Result<(), io::Error> {
-    // This function is called only from within a lock (a dumb compare-and-exchange spinlock). So we can use non-atomic test and set on MY_FILE_OPT.
-    unsafe {
-	if let None = MY_FILE_OPT.as_mut() {
-	    let mut new_file = File::create("smalloclog.log")?;
-	    new_file.write_all(b"i")?;
-
-	    assert!(std::primitive::usize::BITS< 256);
-	    let bytes_in_usize: u8 = (std::primitive::usize::BITS / 8) as u8;
-
-	    new_file.write_all(&[bytes_in_usize])?;
-
-	    MY_FILE_OPT = Some(new_file);
-	};
-
-	MY_FILE_OPT.as_mut().expect("x").write_all(bs)?;
-	Ok(())
-    }
+fn conv(size: u128) -> String {
+    let byte_size = ByteSize::b(size as u64);
+    byte_size.to_string()
 }
+
+use smalloc;
 
 use core::{
     alloc::{GlobalAlloc, Layout}
@@ -37,16 +20,41 @@ use std::sync::atomic::{AtomicBool, Ordering};
 
 static LOCK: AtomicBool = AtomicBool::new(false);
 
-use smalloc::layout_to_sizeclass;
+use smalloc::{layout_to_sizeclass,sizeclass_to_slotsize};
 
 pub struct SmallocLog { }
 
-#[inline(always)]
-fn log_layout(layout: core::alloc::Layout) -> Result<(), io::Error> {
-    write(&layout.size().to_le_bytes())?;
-    write(&layout.align().to_le_bytes())?;
-    Ok(())
+use std::primitive::usize;
+const U_U8: u8 = (std::primitive::usize::BITS / 8) as u8; // number of bytes for a usize
+const U: usize = U_U8 as usize;
+static mut MY_FILE_OPT: Option<File> = None;
+#[allow(static_mut_refs)] // This function is called only from within a lock (a dumb compare-and-exchange spinlock). So we can use non-atomic test and set on MY_FILE_OPT.
+fn create_my_file() -> () {
+    // If MY_FILE_OPT doesn't already have a File in it, create a File, write the smalloclog header to it, and store it in MY_FILE_OPT.
+
+    unsafe {
+	if MY_FILE_OPT.is_none() {
+	    let mut new_file = File::create("smalloclog.log").unwrap();
+
+	    assert!(U <= 32); // looking forward to 256-bit CPUs archs. But not 512-bit CPU archs.
+
+	    let header: [u8; 2] = [
+		b'3', // version of smalloclog file
+		U_U8
+	    ];
+	    new_file.write_all(&header).unwrap();
+
+	    MY_FILE_OPT = Some(new_file);
+	};
+    }
 }
+
+//XXX#[inline(always)]
+//XXXfn log_layout(layout: core::alloc::Layout) -> Result<(), io::Error> {
+//XXX    write(&layout.size().to_le_bytes())?;
+//XXX    write(&layout.align().to_le_bytes())?;
+//XXX    Ok(())
+//XXX}
 
 //XXXX add detection of CPU number
 unsafe impl GlobalAlloc for SmallocLog {
@@ -60,10 +68,23 @@ unsafe impl GlobalAlloc for SmallocLog {
 	    }
 	}
 
-	write(b"a").unwrap();
-	log_layout(layout).unwrap();
 	let p = unsafe { System.alloc(layout) };
-	write(&p.addr().to_le_bytes()).unwrap();
+
+	create_my_file();
+	let mut entry: [u8; 1+3*U] = [0; 1+3*U];
+	let e: &mut [u8] = &mut entry;
+
+	let mut i = 0;
+	e[i] = b'a'; i += 1; // alloc
+	e[i..i+U].copy_from_slice(&layout.size().to_le_bytes()); i += U;
+	e[i..i+U].copy_from_slice(&layout.align().to_le_bytes()); i += U;
+	e[i..i+U].copy_from_slice(&p.addr().to_le_bytes());
+
+	#[allow(static_mut_refs)] // This function is called only from within a lock (a dumb compare-and-exchange spinlock). So we can use non-atomic test and set on MY_FILE_OPT.
+	unsafe {
+	    assert!(! MY_FILE_OPT.is_none(), "It was created by create_MY_FILE above.");
+	    MY_FILE_OPT.as_mut().expect("").write_all(&entry).unwrap();
+	}
 
 	// ok we're done, release the lock
 	LOCK.store(false, Ordering::Release);
@@ -81,8 +102,17 @@ unsafe impl GlobalAlloc for SmallocLog {
 	    }
 	}
 
-	write(b"d").unwrap();
-	write(&ptr.addr().to_le_bytes()).unwrap();
+	let mut entry: [u8; 1+3*U] = [0; 1+3*U];
+	let e: &mut [u8] = &mut entry;
+	let mut i = 0;
+	e[i] = b'd'; i += 1; // dealloc
+	e[i..i+U].copy_from_slice(&ptr.addr().to_le_bytes());
+
+	#[allow(static_mut_refs)] // This function is called only from within a lock (a dumb compare-and-exchange spinlock). So we can use non-atomic test and set on MY_FILE_OPT.
+	unsafe {
+	    assert!(! MY_FILE_OPT.is_none(), "MY_FILE is created in alloc(), which must be called before any call to dealloc().");
+	    MY_FILE_OPT.as_mut().expect("").write_all(&entry).unwrap();
+	}
 
 	// ok we're done, release the lock
 	LOCK.store(false, Ordering::Release);
@@ -100,12 +130,23 @@ unsafe impl GlobalAlloc for SmallocLog {
 	    }
 	}
 
-	write(b"r").unwrap();
-	write(&ptr.addr().to_le_bytes()).unwrap();
-	log_layout(layout).unwrap();
-	write(&new_size.to_le_bytes()).unwrap();
 	let newptr = unsafe { System.realloc(ptr, layout, new_size) };
-	write(&newptr.addr().to_le_bytes()).unwrap();
+
+	let mut entry: [u8; 1+5*U] = [0; 1+5*U];
+	let e: &mut [u8] = &mut entry;
+	let mut i = 0;
+	e[i] = b'r'; i += 1; // realloc
+	e[i..i+U].copy_from_slice(&ptr.addr().to_le_bytes()); i += U;
+	e[i..i+U].copy_from_slice(&layout.size().to_le_bytes()); i += U;
+	e[i..i+U].copy_from_slice(&layout.align().to_le_bytes()); i += U;
+	e[i..i+U].copy_from_slice(&new_size.to_le_bytes()); i += U;
+	e[i..i+U].copy_from_slice(&newptr.addr().to_le_bytes());
+
+	#[allow(static_mut_refs)] // This function is called only from within a lock (a dumb compare-and-exchange spinlock). So we can use non-atomic test and set on MY_FILE_OPT.
+	unsafe {
+	    assert!(! MY_FILE_OPT.is_none(), "MY_FILE is created in alloc(), which must be called before any call to realloc().");
+	    MY_FILE_OPT.as_mut().expect("").write_all(&entry).unwrap();
+	}
 
 	// ok we're done, release the lock
 	LOCK.store(false, Ordering::Release);
@@ -120,12 +161,12 @@ pub fn smalloclog_to_human_readable(sml: &[u8]) -> String {
     let mut i: usize = 0; // index of next byte to read
     let mut res: String = String::new();
 
-    assert!(sml[i] == b'i', "something unexpected in smalloclog");
+    assert!(sml[i] == b'3', "This version of smalloclog can read only version 3 smallocloc files.");
     i += 1;
     let sou: usize = sml[i] as usize; // source usize
     i += 1;
     assert!(sou <= 32); // looking forward to CPU arches with 256-bit pointers
-	
+    
     while i < sml.len() {
 	if sml[i] == b'a' {
 	    i += 1;
@@ -167,8 +208,7 @@ use std::collections::HashMap;
 use thousands::Separable;
 
 pub fn smalloclog_to_stats(sml: &[u8]) -> String {
-    const NUM_SCSU8: u8=30;
-    const NUM_SCS: usize = NUM_SCSU8 as usize;
+    const NUM_SCS: usize = smalloc::NUM_SCSU8 as usize;
     
     let mut slabs_now: Vec<usize> = Vec::with_capacity(NUM_SCS);
     slabs_now.resize(NUM_SCS , 0);
@@ -183,22 +223,18 @@ pub fn smalloclog_to_stats(sml: &[u8]) -> String {
     let mut oversize_totalallocs = 0;
     let mut oversize_highwater = 0;
     
-    let mut realloc_grow = 0;
-    let mut realloc_shrink = 0;
-    let mut realloc_same = 0;
-
     let mut reallocon2c: HashMap<(usize, usize), u64> = HashMap::with_capacity(100_000_000);
     
     let mut ptr2sc: HashMap<usize, u8> = HashMap::with_capacity(10_000_000);
 
     let mut i: usize = 0; // index of next byte to read
 
-    assert!(sml[i] == b'i', "something unexpected in smalloclog");
+    assert!(sml[i] == b'3', "This version of smalloclog can read only version 3 smallocloc files.");
     i += 1;
     let sou: usize = sml[i] as usize; // source usize
     i += 1;
     assert!(sou <= 32); // looking forward to CPU arches with 256-bit pointers
-	
+    
     while i < sml.len() {
 	if sml[i] == b'a' {
 	    i += 1;
@@ -273,13 +309,7 @@ pub fn smalloclog_to_stats(sml: &[u8]) -> String {
 	    assert!(! ptr2sc.contains_key(&newptr));
 	    ptr2sc.insert(newptr, newsc);
 
-	    if newsc > origsc {
-		realloc_grow += 1
-	    } else if newsc < origsc {
-		realloc_shrink += 1
-	    } else {
-		realloc_same += 1
-	    }
+	    //XXX simulate promotion of growers
 
 	    let realloconkey = (siz, newsiz); // XXX note: neglecting alignment here, because it probably won't matter much to the results, and because I can't think of a nice way to take into account while still merging same-sized reallocs...
 	    *reallocon2c.entry(realloconkey).or_insert(0) += 1;
@@ -305,12 +335,13 @@ pub fn smalloclog_to_stats(sml: &[u8]) -> String {
     }
 
     let mut res: String = String::new();
+    writeln!(res, "{:>4} {:>10} {:>9} {:>13}", "sc", "size", "highwater", "tot").unwrap();
+    writeln!(res, "{:>4} {:>10} {:>9} {:>13}", "--", "----", "---------", "---").unwrap();
     for i in 0..NUM_SCS {
-	writeln!(res, "sc: {:>2}, highwater-count: {:>10}, tot: {:>10}", i, slabs_highwater[i].separate_with_commas(), slabs_totallocs[i].separate_with_commas()).unwrap();
+	writeln!(res, "{:>4} {:>10} {:>9} {:>13}", i, conv(sizeclass_to_slotsize(i as u8) as u128), slabs_highwater[i].separate_with_commas(), slabs_totallocs[i].separate_with_commas()).unwrap();
     }
 
-    writeln!(res, "oversize: highwater-count: {:>10}, tot: {:>10}", oversize_highwater.separate_with_commas(), oversize_totalallocs.separate_with_commas()).unwrap();
-    writeln!(res, "realloc grow: {:>6}, shrink: {:>10}, same: {:>10}", realloc_grow.separate_with_commas(), realloc_shrink.separate_with_commas(), realloc_same.separate_with_commas()).unwrap();
+    writeln!(res, ">{:>3} >{:>9} {:>9} {:>13}", NUM_SCS, conv(sizeclass_to_slotsize((NUM_SCS-1) as u8) as u128), oversize_highwater, oversize_totalallocs).unwrap();
 
     let mut reallocon2c_entries: Vec<(&(usize, usize), &u64)> = reallocon2c.iter().collect();
     reallocon2c_entries.sort_by(|a, b| a.1.cmp(b.1));
@@ -333,4 +364,3 @@ pub fn smalloclog_to_stats(sml: &[u8]) -> String {
     writeln!(res, "bytes moved: {}, bytes saved from move: {}", bytes_moved.separate_with_commas(), bytes_saved_from_move.separate_with_commas()).ok();
     res
 }
-
