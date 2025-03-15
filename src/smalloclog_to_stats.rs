cdf7ab3933a3fc6af05f3a338b4d2996ff53c85a
Donald Turnworth <donaldturnworth@Donalds-MacBook-Pro.local>
2025-03-15 14:05:02 -0600

    add buffering to the writing of the smalloclog

--- src/smalloclog_to_stats.rs
+++ src/smalloclog_to_stats.rs
@@ -1,4 +1,3 @@
-
 use std::io::{self, Read};
 
 use smalloclog::smalloclog_to_stats;
@@ -6,9 +5,17 @@ use smalloclog::smalloclog_to_stats;
 //XXXuse firestorm::profile_fn;
 
 fn get_and_print() -> () {
+//XXX    let mut buffer: VecDeque<u8> = VecDeque::with_capacity(2usize.pow(20));
     let mut buffer = Vec::new();
+
+//XXX    let mut buffer: VecDeque<u8> = VecDeque::with_capacity(2usize.pow(20));
+
     let stdin = io::stdin();
     let mut handle = stdin.lock();
+
+//XXX    loop {
+//XXX	buffer.extend_from_slice(xxx);
+//XXX    }
     
     if let Err(e) = handle.read_to_end(&mut buffer) {
         eprintln!("Failed to read from stdin: {}", e);
@@ -18,6 +25,11 @@ fn get_and_print() -> () {
     println!("{}", smalloclog_to_stats(&buffer));
 }
 
+
 fn main() {
     get_and_print();
 }
+
+//XXX#[async_std::main]
+//XXXfn main() -> std::io::Result<()> {
+//XXX};
