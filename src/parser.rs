68d0b8e82a301866ed89f3a32c321c9688683038
Zsomething <nothanks@ontheemail.com>
2025-03-21 17:45:01 -0600

    follow some renamings from smalloc, add tracking over overflows of all-slabs-in-an-area, as well as printout of overflows of one-slab-at-a-time

--- src/parser.rs
+++ src/parser.rs
@@ -45,7 +45,7 @@ use std::collections::{HashMap,HashSet};
 struct ReallocHistoryChapter {
     newsiz: usize,
     align: usize,
-    newsc: usize
+    newslabnum: usize
 }
 
 struct OpenBook {
@@ -76,8 +76,9 @@ impl ClosedBook {
 	let mut movecostsmalloc = 0;
 	for ch in &chs[1..] {
 	    movecostworstperlife += prevch.newsiz;
-	    if ch.newsc != prevch.newsc {
-		movecostsmalloc += prevch.newsiz;
+	    if ch.newslabnum != prevch.newslabnum {
+		// In smalloc, we have to memcpy the entire contents of the previous slot, not just the occupied bytes! (Because we don't know how many are occupied.
+		movecostsmalloc += slabnum_to_slotsize(prevch.newslabnum);
 	    }
 	    prevch = ch;
 	}
@@ -124,13 +125,16 @@ impl Ord for ClosedBook {
 pub struct Statser<W: Write> {
     w: W,
 
-    // How many allocations have been requested out of each of these slabs (each "slab" having a fixed number of fixed-size "slots").
-    slabs_now: Vec<usize>,
-    slabs_totallocs: Vec<usize>,
-    slabs_highwater: Vec<usize>,
+    // How many allocations have been requested out of each of these slabs.
+    allocs_current: Vec<usize>,
+    allocs_total: Vec<usize>,
+    allocs_highwater: Vec<usize>,
 
-    // map ptr to current SC
-    ptr2sc: HashMap<usize, usize>,
+    // How many times have all the slabs in a whole area been found to be full when we were trying to insert into it.
+    slabareaoverflows: Vec<usize>,
+
+    // map ptr to current slab num
+    ptr2slabnum: HashMap<usize, usize>,
 
     // map current ptr to its so-far life history of reallocs
     ptr2ob: HashMap<usize, OpenBook>,
@@ -147,39 +151,48 @@ fn conv(size: usize) -> String {
 }
 use thousands::Separable;
 
-use smalloc::{sizeclass_to_numslots, sizeclass_to_slotsize, OVERSIZE_SC, NUM_SCS, MAX_SC_TO_PACK_INTO_CACHELINE, HUGE_SLOTS_SC};
+use smalloc::{slabnum_to_numareas, slabnum_to_numslots, slabnum_to_slotsize, OVERSIZE_SLABNUM, NUM_SLABS, MAX_SLABNUM_TO_FIT_INTO_CACHELINE, LARGE_SLOTS_SLABNUM};
+
+fn tot_slots(slabnum: usize) -> usize {
+    slabnum_to_numareas(slabnum) * slabnum_to_numslots(slabnum)
+}
 
 impl<W:Write> Statser<W> {
 
     pub fn new(w: W) -> Self {
 	let mut ns = Statser {
 	    w,
-	    slabs_now: Vec::with_capacity(NUM_SCS),
-	    slabs_totallocs: Vec::with_capacity(NUM_SCS),
-	    slabs_highwater: Vec::with_capacity(NUM_SCS),
-	    ptr2sc: HashMap::with_capacity(100_000_000),
+	    allocs_current: Vec::with_capacity(NUM_SLABS),
+	    allocs_total: Vec::with_capacity(NUM_SLABS),
+	    allocs_highwater: Vec::with_capacity(NUM_SLABS),
+	    slabareaoverflows: Vec::with_capacity(NUM_SLABS),
+	    ptr2slabnum: HashMap::with_capacity(100_000_000),
 	    ptr2ob: HashMap::with_capacity(100_000_000),
 	    cbs: HashSet::with_capacity(100_000_000)
 	};
 
-	ns.slabs_now.resize(NUM_SCS, 0); // initialize elements to 0
-	ns.slabs_totallocs.resize(NUM_SCS, 0); // initialize elements to 0
-	ns.slabs_highwater.resize(NUM_SCS, 0); // initialize elements to 0
+	ns.allocs_current.resize(NUM_SLABS, 0); // initialize elements to 0
+	ns.allocs_total.resize(NUM_SLABS, 0); // initialize elements to 0
+	ns.allocs_highwater.resize(NUM_SLABS, 0); // initialize elements to 0
+	ns.slabareaoverflows.resize(NUM_SLABS, 0); // initialize elements to 0
 
-	eprintln!("{:>4} {:>11} {:>11} {:>11} {:>15}", "sc", "size", "slots", "highwater", "tot");
-	eprintln!("{:>4} {:>11} {:>11} {:>11} {:>15}", "--", "----", "-----", "---------", "---");
+	eprintln!("{:>5} {:>8} {:>11} {:>11} {:>7} {:>7} {:>15}", "slab#", "size", "slots", "highwater", "overs", "overa", "tot");
+	eprintln!("{:>5} {:>8} {:>11} {:>11} {:>7} {:>7} {:>15}", "-----", "----", "-----", "---------", "-----", "-----", "---");
 
 	ns
     }
 
     fn write_stats(&mut self) {
-	writeln!(self.w, "{:>4} {:>11} {:>11} {:>11} {:>15}", "sc", "size", "slots", "highwater", "tot").unwrap();
-	writeln!(self.w, "{:>4} {:>11} {:>11} {:>11} {:>15}", "--", "----", "-----", "---------", "---").unwrap();
-	for i in 0..OVERSIZE_SC {
-	    writeln!(self.w, "{:>4} {:>11} {:>11} {:>11} {:>15}", i, conv(sizeclass_to_slotsize(i)), sizeclass_to_numslots(i).separate_with_commas(), self.slabs_highwater[i].separate_with_commas(), self.slabs_totallocs[i].separate_with_commas()).unwrap();
+	writeln!(self.w, "{:>5} {:>8} {:>11} {:>11} {:>7} {:>7} {:>15}", "slab#", "size", "slots", "highwater", "overf", "overa", "tot").unwrap();
+	writeln!(self.w, "{:>5} {:>8} {:>11} {:>11} {:>7} {:>7} {:>15}", "-----", "----", "-----", "---------", "-----", "-----", "---").unwrap();
+	for i in 0..OVERSIZE_SLABNUM {
+	    let hw = self.allocs_highwater[i];
+	    let slotsperslab = slabnum_to_numslots(i);
+	    let overf = hw / slotsperslab;
+	    writeln!(self.w, "{:>5} {:>8} {:>11} {:>11} {:>7} {:>7} {:>15}", i, conv(slabnum_to_slotsize(i)), slotsperslab.separate_with_commas(), hw.separate_with_commas(), overf, self.slabareaoverflows[i], self.allocs_total[i].separate_with_commas()).unwrap();
 	}
 
-	writeln!(self.w, ">{:>3} >{:>10} >{:>10} {:>11} {:>15}", OVERSIZE_SC-1, conv(sizeclass_to_slotsize(OVERSIZE_SC-1)), sizeclass_to_numslots(OVERSIZE_SC-1).separate_with_commas(), self.slabs_highwater[OVERSIZE_SC].separate_with_commas(), self.slabs_totallocs[OVERSIZE_SC].separate_with_commas()).unwrap();
+	writeln!(self.w, "  >{:>2} >{:>7} >{:>10} {:>11} ({:>5}) ({:>5}) {:>15}", OVERSIZE_SLABNUM-1, conv(slabnum_to_slotsize(OVERSIZE_SLABNUM-1)), slabnum_to_numslots(OVERSIZE_SLABNUM-1).separate_with_commas(), self.allocs_highwater[OVERSIZE_SLABNUM].separate_with_commas(), "N/A", "N/A", self.allocs_total[OVERSIZE_SLABNUM].separate_with_commas()).unwrap();
 
 	let mut tot_bytes_worst = 0;
 	let mut tot_bytes_smalloc = 0;
@@ -191,8 +204,7 @@ impl<W:Write> Statser<W> {
 	for cb in cbs {
 	    let mcw = cb.movecostworstperlife * cb.numlifes;
 	    let mcs = cb.movecostsmalloc;
-	    assert!(mcw >= mcs);
-	    let saved = mcw-mcs;
+	    let saved = mcw as i64 - mcs as i64;
 	    let percsaved = (saved as f64 / mcw as f64) * 100.0;
             writeln!(self.w, "{:>13} {:>14} {:>14} {:>14} {:>5.0}% {:<14}", cb.numlifes.separate_with_commas(), mcw.separate_with_commas(), mcs.separate_with_commas(), saved.separate_with_commas(), percsaved, cb.key).ok();
 	    tot_bytes_worst += mcw;
@@ -203,50 +215,36 @@ impl<W:Write> Statser<W> {
 	let totalpercsaved = (tot_bytes_saved as f64 / tot_bytes_worst as f64) * 100.0;
 	writeln!(self.w, "tot bytes moved worst-case: {:>14}, tot bytes moved smalloc: {:>14}, saved from move: {:>14}, percentage saved: {:>3.0}%", tot_bytes_worst.separate_with_commas(), tot_bytes_smalloc.separate_with_commas(), tot_bytes_saved.separate_with_commas(), totalpercsaved).ok();
     }
-
-    fn find_next_size_class_with_open_slot(&mut self, mut sc: usize) -> usize {
-	let mut s = sizeclass_to_numslots(sc);
-	
-	assert!(self.slabs_now[sc] <= s, "{}, {}, {}", sc, self.slabs_now[sc], s); // We can never have more that s slots in a slab.
-
-	// Note that in smalloc we falsely claim that the "oversize" slab has 8 bytes worth of indexes, when in fact there is no slab there, we're just going to fall back to mmap() for things that big. We just pretend there are 2^64 slots in that slab so that this will never overflow out of that sizeclass.
-	while self.slabs_now[sc] == s {
-	    // This slab is full so we overflow to the next sizeclass.
-
-	    sc += 1;
-	    assert!(sc < NUM_SCS, "sc: {}, NUM_SCS: {}, slabs[{}]: {}", sc, NUM_SCS, sc-1, self.slabs_now[sc-1]); // See "Note" above, on this loop.
-
-	    s = sizeclass_to_numslots(sc);
-
-	    assert!(self.slabs_now[sc] <= s, "{}, {}, {}", sc, self.slabs_now[sc], s); // We can never have more that s slots in a slab.
-	}
-		
-	assert!(self.slabs_now[sc] < s);
-
-	sc
-    }
 }
 
 use std::cmp::max;
-use smalloc::{layout_to_sizeclass};
+use smalloc::{layout_to_slabnum};
 impl<W: Write> EntryConsumerTrait for Statser<W> {
     fn consume_entry(&mut self, e: &Entry) {
 	match e {
 	    Entry::Alloc { reqsiz, reqalign, resptr } => {
-		let prevsc = layout_to_sizeclass(*reqsiz, *reqalign);
-		assert!(prevsc < NUM_SCS);
+		let mut slabnum = layout_to_slabnum(*reqsiz, *reqalign);
+		assert!(slabnum < NUM_SLABS);
 
 		// Overflow of slabs:
-		let sc = self.find_next_size_class_with_open_slot(prevsc);
+		// If all slabs of this size in all areas are full...
+		assert!(self.allocs_current[slabnum] <= tot_slots(slabnum));
+		while self.allocs_current[slabnum] == tot_slots(slabnum) {
+		    // ... Then move to a new slabnum
+		    self.slabareaoverflows[slabnum] += 1;
+		    slabnum += 1;
+		    assert!(slabnum < NUM_SLABS, "slabnum: {}, NUM_SLABS: {}, slabs[{}]: {}", slabnum, NUM_SLABS, slabnum-1, self.allocs_current[slabnum-1]); // Note that in smalloc we falsely claim that the "oversize" slab has 7 bytes worth of indexes, when in fact there is no slab there, we're just going to fall back to mmap() for things that big. We just pretend there are 2^64 slots in that slab so that this simulation will never overflow out of that slabnum.
+		}
+		assert!(self.allocs_current[slabnum] < tot_slots(slabnum));
 
-		assert!(! self.ptr2sc.contains_key(resptr));
-		self.ptr2sc.insert(*resptr, sc);
+		assert!(! self.ptr2slabnum.contains_key(resptr));
+		self.ptr2slabnum.insert(*resptr, slabnum);
 		assert!(! self.ptr2ob.contains_key(resptr));
 
 		let ch: ReallocHistoryChapter = ReallocHistoryChapter {
 		    newsiz: *reqsiz,
 		    align: *reqalign,
-		    newsc: sc
+		    newslabnum: slabnum
 		};
 
 		let ob: OpenBook = OpenBook {
@@ -255,114 +253,120 @@ impl<W: Write> EntryConsumerTrait for Statser<W> {
 
 		self.ptr2ob.insert(*resptr, ob);
 
-		self.slabs_totallocs[sc] += 1;
-		self.slabs_now[sc] += 1;
+		self.allocs_total[slabnum] += 1;
+		self.allocs_current[slabnum] += 1;
 
-		if self.slabs_now[sc] > self.slabs_highwater[sc] {
-		    self.slabs_highwater[sc] = self.slabs_now[sc];
-		    if self.slabs_now[sc] == sizeclass_to_numslots(sc) {
-			if sc == OVERSIZE_SC {
-			    eprintln!(">{:>3} >{:10} >{:>10} {:>11} {:>15}", OVERSIZE_SC-1, conv(sizeclass_to_slotsize(OVERSIZE_SC-1)), sizeclass_to_numslots(OVERSIZE_SC-1).separate_with_commas(), self.slabs_highwater[sc].separate_with_commas(), self.slabs_totallocs[sc].separate_with_commas());
-			} else {
-			    eprintln!("{:>4} {:>11} {:>11} {:>11} {:>15}", sc, conv(sizeclass_to_slotsize(sc)), sizeclass_to_numslots(sc).separate_with_commas(), self.slabs_highwater[sc].separate_with_commas(), self.slabs_totallocs[sc].separate_with_commas());
-			}
+		if self.allocs_current[slabnum] > self.allocs_highwater[slabnum] {
+		    self.allocs_highwater[slabnum] = self.allocs_current[slabnum];
+		    let numslotsperslabonearea = slabnum_to_numslots(slabnum);
+		    let numslotsperslaballareas = numslotsperslabonearea * slabnum_to_numareas(slabnum);
+		    if self.allocs_current[slabnum] % numslotsperslabonearea == 0 {
+			eprintln!("{:>5} {:>8} {:>11} {:>11} {:>7} {:>7} {:>15}", slabnum, conv(slabnum_to_slotsize(slabnum)), numslotsperslaballareas.separate_with_commas(), self.allocs_highwater[slabnum].separate_with_commas(), self.allocs_current[slabnum] / numslotsperslabonearea, self.slabareaoverflows[slabnum], self.allocs_total[slabnum].separate_with_commas());
 		    }
 		}
 	    }
 
 	    Entry::Free { oldptr } => {
-		assert!(self.ptr2sc.contains_key(oldptr));
+		assert!(self.ptr2slabnum.contains_key(oldptr));
 		assert!(self.ptr2ob.contains_key(oldptr));
 
-		let sc = self.ptr2sc[oldptr];
-		assert!(sc < NUM_SCS);
+		let slabnum = self.ptr2slabnum[oldptr];
+		assert!(slabnum < NUM_SLABS);
 
-		assert!(self.slabs_now[sc] > 0, "slabs[{}]: {}", sc, self.slabs_now[sc]);
-		self.slabs_now[sc] -= 1;
+		assert!(self.allocs_current[slabnum] > 0, "allocs_current[{}]: {}", slabnum, self.allocs_current[slabnum]);
+		self.allocs_current[slabnum] -= 1;
 
-		self.ptr2sc.remove(oldptr);
+		self.ptr2slabnum.remove(oldptr);
 
 		// Now we need to collect realloc-history statistics for later reporting before we free this.
 		let ob = self.ptr2ob.remove(oldptr).unwrap();
 		let cb = ClosedBook::new(ob);
 
 		// But you know what? If the worst-case cost of moved bytes in this life story was 0, then nobody cares so optimize it out...
-		if cb.movecostworstperlife > 0 {
-		    // Add this history into our set, summing it with any existing matching histories.
-		    let curcbopt = self.cbs.take(&cb);
+		if cb.movecostworstperlife == 0 {
+		    return
+		}
 		
-		    if curcbopt.is_some() {
-			let mut curcb = curcbopt.unwrap();
+		// Add this history into our set, summing it with any existing matching histories.
+		let curcbopt = self.cbs.take(&cb);
+		
+		if curcbopt.is_some() {
+		    let mut curcb = curcbopt.unwrap();
 
-			curcb.movecostsmalloc += cb.movecostsmalloc;
-			curcb.numlifes += 1;
-
-			self.cbs.insert(curcb);
-		    } else {
-			self.cbs.insert(cb);
-		    }
+		    curcb.movecostsmalloc += cb.movecostsmalloc;
+		    curcb.numlifes += 1;
+		    
+		    self.cbs.insert(curcb);
+		} else {
+		    self.cbs.insert(cb);
 		}
 	    }
 
 	    Entry::Realloc { prevptr, prevsiz, reqalign, newsiz, resptr } => {
-		assert!(self.ptr2sc.contains_key(prevptr));
+		assert!(self.ptr2slabnum.contains_key(prevptr));
 		assert!(self.ptr2ob.contains_key(prevptr));
 
-		let prevsc = self.ptr2sc[prevptr];
-		assert!(prevsc < NUM_SCS);
-		assert!(layout_to_sizeclass(*prevsiz, *reqalign) <= prevsc); // The SC we had this ptr in (in ptr2sc) was big enough to hold the prevsiz&alignment.
-		self.ptr2sc.remove(prevptr);
+		let prevslabnum = self.ptr2slabnum[prevptr];
+		assert!(prevslabnum < NUM_SLABS);
+		assert!(layout_to_slabnum(*prevsiz, *reqalign) <= prevslabnum); // The slab num we had this ptr in (in ptr2slabnum) was big enough to hold the prevsiz&alignment.
+		self.ptr2slabnum.remove(prevptr);
 
-		self.slabs_now[prevsc] -= 1;
+		self.allocs_current[prevslabnum] -= 1;
 
-		// Now we simulate two things: 1. Promotion of growers. 2. Overflow of slabs, By "simulate" I mean that while the actual underlying allocator is going to do whatever it does with this request for realloc(), we're here going to choose a sizeclass to simulate that the new pointer would be in in smalloc.
+		// Now we simulate two things: 1. Promotion of growers. 2. Overflow of slabs, By "simulate" I mean that while the actual underlying allocator is going to do whatever it does with this request for realloc(), we're here going to choose a slabnum to simulate that the new pointer would be in in smalloc.
 
-		// XXX (Note: we're assuming here the "worst-case scenario", where one CPU did all of these allocations. In practice we may get a little relief of the congestion of these slabs from the allocations being spread out over multiple CPUs, but we don't want to count on that necessarily, so let's look at the worst-case scenario first...)
+		let mut newslabnum = max(prevslabnum, layout_to_slabnum(*newsiz, *reqalign));
+		assert!(newslabnum < NUM_SLABS);
+		assert!(newslabnum >= prevslabnum);
 
-		// 1. Promote growers. Any re-allocation which exceeds its slot gets bumped--not to the next sizeclass that is just big enough--but on to the next sizeclass *group*. The groups are: A. Things that can pack into a 64-byte cache line, B. Things that can pack into a 4096-byte memory page, and C. The huge slots slab. Grower promotion can promote an allocation to from group A to group B, or from group B to group C.
-		let mut newsc = max(prevsc, layout_to_sizeclass(*newsiz, *reqalign));
-		assert!(newsc < NUM_SCS);
-		assert!(newsc >= prevsc);
-
-		if newsc > prevsc {
+		// 1. Promote growers. Any re-allocation whose new requested size exceeds its slot size gets bumped--not to the next slabnum that is just big enough--but on to slab 14, i.e. the slots just big enough to fit into one (standard) cacheline. If it is too big too fit into 64 bytes, then just promote it directly to the "large" size (<= 6,000,000 bytes).
+		if newslabnum > prevslabnum {
 		    // Okay this realloc required moving the object to a bigger slab. Therefore this is a "grower".
-		    if newsc <= MAX_SC_TO_PACK_INTO_CACHELINE {
-			newsc = MAX_SC_TO_PACK_INTO_CACHELINE + 1;
+		    if newslabnum <= MAX_SLABNUM_TO_FIT_INTO_CACHELINE {
+			newslabnum = MAX_SLABNUM_TO_FIT_INTO_CACHELINE;
 		    } else {
-			newsc = HUGE_SLOTS_SC;
+			newslabnum = LARGE_SLOTS_SLABNUM;
 		    }
 		}
 
 		// 2. Overflow of slabs:
-		let newsc = self.find_next_size_class_with_open_slot(newsc);
+		// If all slabs of this size in all areas are full...
+		assert!(self.allocs_current[newslabnum] <= tot_slots(newslabnum));
+		while self.allocs_current[newslabnum] == tot_slots(newslabnum) {
+		    // ... Then move to a new slabnum
+		    self.slabareaoverflows[newslabnum] += 1;
+		    newslabnum += 1;
+		    assert!(newslabnum < NUM_SLABS, "newslabnum: {}, NUM_SLABS: {}, allocs_current[{}]: {}", newslabnum, NUM_SLABS, newslabnum-1, self.allocs_current[newslabnum-1]); // Note that in smalloc we falsely claim that the "oversize" slab has 7 bytes worth of indexes, when in fact there is no slab there, we're just going to fall back to mmap() for things that big. We just pretend there are 2^64 slots in that slab so that this simulation will never overflow out of that slabnum.
+		}
+		assert!(self.allocs_current[newslabnum] < tot_slots(newslabnum));
 
-		// Okay now insert this into our map from (new) pointer to sc.
-		assert!(! self.ptr2sc.contains_key(resptr));
-		self.ptr2sc.insert(*resptr, newsc);
+		// Okay now insert this into our map from (new) pointer to newslabnum.
+		assert!(! self.ptr2slabnum.contains_key(resptr));
+		self.ptr2slabnum.insert(*resptr, newslabnum);
 
 		// Move this allocation's reallocation history to the new ptr in our map.
 		let mut ob = self.ptr2ob.remove(prevptr).unwrap();
 		// And append this event to its reallocation history, but only if it is a realloc to larger.
 		if newsiz > prevsiz {
 		    ob.chs.push(ReallocHistoryChapter {
-			newsiz: *newsiz, align: *reqalign, newsc
+			newsiz: *newsiz, align: *reqalign, newslabnum
 		    });
 		}
 		self.ptr2ob.insert(*resptr, ob);
 
-		if prevsc != newsc {
-		    self.slabs_totallocs[newsc] += 1;
+		if prevslabnum != newslabnum {
+		    self.allocs_total[newslabnum] += 1;
 		}
 
-		self.slabs_now[newsc] += 1;
-		if self.slabs_now[newsc] > self.slabs_highwater[newsc] {
-		    self.slabs_highwater[newsc] = self.slabs_now[newsc];
+		self.allocs_current[newslabnum] += 1;
+		if self.allocs_current[newslabnum] > self.allocs_highwater[newslabnum] {
+		    self.allocs_highwater[newslabnum] = self.allocs_current[newslabnum];
 
-		    if self.slabs_now[newsc] == sizeclass_to_numslots(newsc) {
-			if newsc == OVERSIZE_SC {
-			    eprintln!(">{:>3} >{:10} >{:>10} {:>11} {:>15}", OVERSIZE_SC-1, conv(sizeclass_to_slotsize(OVERSIZE_SC-1)), sizeclass_to_numslots(OVERSIZE_SC-1), self.slabs_highwater[newsc].separate_with_commas(), self.slabs_totallocs[newsc].separate_with_commas());
+		    if self.allocs_current[newslabnum] == slabnum_to_numslots(newslabnum) {
+			if newslabnum == OVERSIZE_SLABNUM {
+			    eprintln!(">{:>3} >{:10} >{:>10} {:>11} {:>15}", OVERSIZE_SLABNUM-1, conv(slabnum_to_slotsize(OVERSIZE_SLABNUM-1)), slabnum_to_numslots(OVERSIZE_SLABNUM-1), self.allocs_highwater[newslabnum].separate_with_commas(), self.allocs_total[newslabnum].separate_with_commas());
 			} else {
-			    eprintln!("{:>4} {:>11} {:>11} {:>11} {:>15}", newsc, conv(sizeclass_to_slotsize(newsc)), sizeclass_to_numslots(newsc), self.slabs_highwater[newsc].separate_with_commas(), self.slabs_totallocs[newsc].separate_with_commas());
+			    eprintln!("{:>4} {:>11} {:>11} {:>11} {:>15}", newslabnum, conv(slabnum_to_slotsize(newslabnum)), slabnum_to_numslots(newslabnum), self.allocs_highwater[newslabnum].separate_with_commas(), self.allocs_total[newslabnum].separate_with_commas());
 			}
 		    }
 		}
